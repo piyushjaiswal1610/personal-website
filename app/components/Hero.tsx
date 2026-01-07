97f0f501ba230868d410a0ee472227acef1fc1c6
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2026-01-07 22:47:46 +0530

    feat: adds hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center absolute h-full w-full">
+      <h1 className="text-4xl font-bold">I&apos;m Piyush</h1>
+      <h3 className="text-2xl font-medium my-2">I work on backend systems.</h3>
+      <p className="text-md text-gray-500 italic">
+        APIs, services, and learning my way into DevOps and infrastructure.
+      </p>
+    </div>
+  );
+}
